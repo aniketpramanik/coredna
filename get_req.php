2b1bd2012002de7cb359efc7e3712c02ddef98e4
aniketmapsslink <aniket@mapsslink.com>
2023-01-07 22:00:27 +1100

    Final Commit

---
+++ get_req.php
@@ -0,0 +1,24 @@
+<?php
+//Payload to be post to the end point
+$payload = json_encode([
+    "name" => "Aniket Pramanik",
+    "email" => "aniketlpuan@gmail.com",
+    "url" => "https://github.com/aniketpramanik/coredna.git"
+]);
+
+$options = [
+    "http" =>[
+        "method" => "GET",
+        "header" => "Authorization: Bearer d417fcbc-94b7-4357-aff3-536c33364428; Content-type: application/json; charset = UTF-8; ",// Bearer token authorization
+        "content" => $payload
+    ]
+
+    ];
+
+    $data = file_get_contents("https://corednacom.corewebdna.com/assessment-endpoint.php", false,
+                                        $context);
+
+    var_dump($payload); //Showing Paylaod
+    print_r($http_response_header[0]); // Displaying the response from the api.
+
+?>
